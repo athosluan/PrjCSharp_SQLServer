0b6f03113d10b1ec22268db9dd36cdf8ebb01bfe
Athos <athosluan@gmail.com>
2021-02-04 11:56:10 -0300

    Commit inicial

---
+++ EstudoGridView/EstudoGridView.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace EstudoGridView
+{
+    partial class EstudoGridView
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.btCarregar = new System.Windows.Forms.Button();
+            this.btFiltro = new System.Windows.Forms.Button();
+            this.txtFiltro = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRegistros = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 142);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(938, 383);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(929, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(93, 116);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(115, 20);
+            this.txtQuantidade.TabIndex = 3;
+            this.txtQuantidade.Text = "Qtd Registros";
+            // 
+            // btCarregar
+            // 
+            this.btCarregar.Location = new System.Drawing.Point(12, 113);
+            this.btCarregar.Name = "btCarregar";
+            this.btCarregar.Size = new System.Drawing.Size(75, 23);
+            this.btCarregar.TabIndex = 4;
+            this.btCarregar.Text = "Carregar:";
+            this.btCarregar.UseVisualStyleBackColor = true;
+            this.btCarregar.Click += new System.EventHandler(this.btCarregar_Click);
+            // 
+            // btFiltro
+            // 
+            this.btFiltro.Location = new System.Drawing.Point(422, 114);
+            this.btFiltro.Name = "btFiltro";
+            this.btFiltro.Size = new System.Drawing.Size(75, 23);
+            this.btFiltro.TabIndex = 6;
+            this.btFiltro.Text = "Carregar:";
+            this.btFiltro.UseVisualStyleBackColor = true;
+            this.btFiltro.Click += new System.EventHandler(this.btFiltro_Click);
+            // 
+            // txtFiltro
+            // 
+            this.txtFiltro.Location = new System.Drawing.Point(503, 117);
+            this.txtFiltro.Name = "txtFiltro";
+            this.txtFiltro.Size = new System.Drawing.Size(157, 20);
+            this.txtFiltro.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(859, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Registros:";
+            // 
+            // lblRegistros
+            // 
+            this.lblRegistros.AutoSize = true;
+            this.lblRegistros.Location = new System.Drawing.Point(931, 113);
+            this.lblRegistros.Name = "lblRegistros";
+            this.lblRegistros.Size = new System.Drawing.Size(10, 13);
+            this.lblRegistros.TabIndex = 8;
+            this.lblRegistros.Text = ".";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(503, 90);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(157, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // EstudoGridView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 537);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblRegistros);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btFiltro);
+            this.Controls.Add(this.txtFiltro);
+            this.Controls.Add(this.btCarregar);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "EstudoGridView";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Button btCarregar;
+        private System.Windows.Forms.Button btFiltro;
+        private System.Windows.Forms.TextBox txtFiltro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRegistros;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ EstudoGridView/EstudoGridView.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using FirebirdSql.Data.FirebirdClient;
+
+namespace EstudoGridView
+{
+    public partial class EstudoGridView : Form
+    {
+        public EstudoGridView()
+        {
+            InitializeComponent();
+        }
+
+        int QtdRegistros { get; set; }
+        DataSet Ds;
+        SqlDataAdapter sda;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string StrSelect = textBox1.Text;
+            SqlConnection con = new SqlConnection("Data Source=localhost;Initial Catalog=New_Veletrica;User id=user;Password=pwd");
+            Ds = new DataSet();
+            con.Open();
+            sda = new SqlDataAdapter(StrSelect, con);
+            sda.Fill(Ds, "Clientes");
+            button1.Text = string.Concat("Conectado na Base ", con.Database);
+            con.Close();
+            con.Dispose();
+            sda.Dispose();
+        }        
+
+        private void ContaRegistros()
+        {
+            QtdRegistros = this.dataGridView1.Rows.Count;
+            lblRegistros.Text = QtdRegistros.ToString();
+        }
+
+
+        private void btCarregar_Click(object sender, EventArgs e)
+        {
+            int qtd = int.Parse(txtQuantidade.Text);
+
+            DataTable dt = new DataTable();
+
+            foreach(DataColumn OldCol in Ds.Tables[0].Columns)
+            {
+                DataColumn dcNew = new DataColumn();
+                dcNew.ColumnName = OldCol.ColumnName;
+                dcNew.DataType = OldCol.DataType;
+                dcNew.Caption = OldCol.Caption;
+                dcNew.MaxLength = OldCol.MaxLength;
+                dt.Columns.Add(dcNew);
+            }
+            
+            for (int i = 0; i < qtd; i++)
+            {
+                DataRow item = Ds.Tables[0].Rows[i];
+                dt.ImportRow(item);
+            }
+            
+            dataGridView1.DataSource = dt;
+            dt.Dispose();
+            ContaRegistros();
+        }
+
+        private void btFiltro_Click(object sender, EventArgs e)
+        {
+            DataView Dataview = new DataView();
+            Dataview.Table = Ds.Tables[0];
+            if (!string.IsNullOrWhiteSpace(txtFiltro.Text))
+            {                
+                Dataview.RowFilter = txtFiltro.Text;
+                Dataview.RowStateFilter = DataViewRowState.CurrentRows;
+            }
+            else
+            {
+                Dataview.RowFilter = "";
+                Dataview.RowStateFilter = DataViewRowState.OriginalRows;
+            }
+            this.dataGridView1.DataSource = Dataview;
+            ContaRegistros();
+        }
+    }
+}
